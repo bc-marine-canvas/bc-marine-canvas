616256aa08b969fca6624bc239587c18d49a1785
James Boynton <james.boynton0@gmail.com>
2021-10-05 06:22:31 -0400

    Fix path definitions in config initialization

--- config/environment.php
+++ config/environment.php
@@ -2,7 +2,13 @@
 
 use Dotenv\Dotenv;
 
-require_once 'define_paths.php';
+// Define project paths
+define('ROOT_DIR', '/var/www');
+define('DOC_ROOT_DIR', '/var/www/app');
+define('CONFIG_DIR', '/var/www/config');
+define('ENV_DIR', '/var/www/config/environments');
+define('ENV_FILE', '/var/www/config/.env');
+define('CONTENT_DIR', '/var/www/app/content');
 
 // Load environment variables
 $env = Dotenv::createUnsafeImmutable(CONFIG_DIR);
